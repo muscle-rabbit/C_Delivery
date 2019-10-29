9ec747fa39a80fc2b5a542fe708e6134ce241500
水野慎也 <mizunoshinya@mizunoshinyanoMacBook-puro.local>
2019-10-29 13:01:48 +0900

    initial commit

---
+++ src/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/shinyamizuno1008/delivery-chuo/src/messages"
+)
+
+var bot *linebot.Client
+
+func main() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				quota, err := bot.GetMessageQuota().Do()
+				if err != nil {
+					log.Println("Quota err:", err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, messages.ReplyThankYou(bot)).Do(); err != nil {
+					log.Print(err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, messages.ReplyConfirmationText(bot), messages.ReplyConfirmationButton(bot)).Do(); err != nil {
+					log.Print(err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, messages.ReplyMenu(bot)).Do(); err != nil {
+					log.Print(err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, messages.ReplyReservationDate(bot)).Do(); err != nil {
+					log.Print(err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, messages.ReplyReservationTime(bot)).Do(); err != nil {
+					log.Print(err)
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+}
