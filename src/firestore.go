b5a29f67ef121f06f50010243da3ff07063141a4
水野慎也 <mizunoshinya@mizunoshinyanoMacBook-puro.local>
2019-11-04 19:09:27 +0900

    firestore のユーザーID取得ロジックを変更しました。

--- src/firestore.go
+++ src/firestore.go
@@ -11,6 +11,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -18,6 +19,7 @@ import (
 type User struct {
 	UserID      string `firestore:"user_id,omitempty"`
 	DisplayName string `firestore:"display_name,omitempty"`
+	CreatedAt   string `firestore:"created_at,omitempty"`
 }
 
 func newApp() (*app, error) {
@@ -53,26 +55,43 @@ func (app *app) addUser(profile *linebot.UserProfileResponse) (string, error) {
 
 	// user がすでに登録されていたら nil を返す。
 	iter := client.Collection("users").Where("user_id", "==", profile.UserID).Documents(ctx)
-	doc, err := iter.Next()
-	if err != nil {
-		if doc == nil {
-			user := User{UserID: profile.UserID, DisplayName: profile.DisplayName}
-			ref, _, err := client.Collection("users").Add(ctx, user)
-			if err != nil {
-				return "", fmt.Errorf("couldn't find user document ref: %v", err)
-			}
-			return ref.ID, nil
-		} else {
-			return "", fmt.Errorf("couldn't iterate user document: %v", err)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
 		}
+		if err != nil {
+			return "", err
+		}
+		return doc.Ref.ID, nil
 	}
 
-	return doc.Ref.ID, nil
+	return "", nil
 }
 
 // TODO: 注文を追加するメソッドの追加 Create
 func (app *app) addOrder() error {
-	var err error
+	ctx := context.Background()
+	client, err := app.client.Firestore(ctx)
+	if err != nil {
+		return fmt.Errorf("couldn't create client in addUser: %v", err)
+	}
+	fmt.Println(client)
+
+	// user がすでに登録されていたら nil を返す。
+	// doc, err := iter.Next()
+	// if err != nil {
+	// 	if doc == nil {
+	// 		user := User{UserID: profile.UserID, DisplayName: profile.DisplayName, CreatedAt: time.Now()}
+	// 		ref, _, err := client.Collection("orders").Add(ctx, user)
+	// 		if err != nil {
+	// 			return "", fmt.Errorf("couldn't find user document ref: %v", err)
+	// 		}
+	// 		return ref.ID, nil
+	// 	} else {
+	// 		return "", fmt.Errorf("couldn't iterate user document: %v", err)
+	// 	}
+	// }
 
 	return err
 }
